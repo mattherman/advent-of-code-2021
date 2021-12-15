28ca2440bf1a5480b98ba4ea7fdddfa4f4b1b26e
Matthew Herman <mattherman11@gmail.com>
2021-12-15 17:10:48 -0600

    Closer

--- 04/Exercise1.fsx
+++ 04/Exercise1.fsx
@@ -36,11 +36,37 @@ let parseBingoInput lines =
         |> List.tail
         |> List.filter (fun l -> l <> "")
         |> List.map parseBoardLine
-        |> List.windowed 5
+        |> List.windowed 5 // TODO - this doesn't actually split the list
     { Numbers = numbers; Boards = boards; CalledNumbers = [] }
 
 let isWinningBoard (calledNumbers: int list) (board: Board) =
-    false // TODO
+    let completedSets (sets: int list list) =
+        sets
+        |> List.map (fun set -> set |> List.except calledNumbers) 
+        |> List.filter (fun remainingNumbers -> remainingNumbers.Length = 0)
+
+    let rows = board
+    let winningHorizontalPositions = rows |> completedSets
+
+    // [[ a b c d e ]
+    //  [ f g h i j ]
+    //  [ k l m n o ]
+    //  [ p q r s t ]
+    //  [ u v w x y ]] -->
+    //                    |
+    // [[ a f k p u ]  <--
+    //  [ b g l q v ]
+    //  [ c h m r w ]
+    //  [ d i n s x ]
+    //  [ e j o t y ]]
+    let columns = seq {
+        for column in 0 .. 4 do
+            board
+            |> Seq.map (fun row -> row.[column])
+            |> Seq.toList
+    }
+    let winningVerticalPositions = columns |> Seq.toList |> completedSets
+    winningHorizontalPositions.Length > 0 || winningVerticalPositions.Length > 0
 
 let getWinningBoards (calledNumbers: int list) (candidateBoards: Board list) =
     candidateBoards |> List.filter (isWinningBoard calledNumbers)
@@ -50,21 +76,24 @@ let callNextNumber (state: BingoState) =
     | nextNumber::remainingNumbers ->
         let calledNumbers = nextNumber::state.CalledNumbers
         let updatedState = { state with Numbers = remainingNumbers; CalledNumbers = calledNumbers }
-        Some (nextNumber, updatedState)
+        Some updatedState
     | [] ->
         None
 
 let startGame (initialState: BingoState) =
     let rec play (state: BingoState) =
         callNextNumber state
-        |> Option.bind(fun (calledNumber, newState) ->
+        |> Option.bind(fun newState ->
             let winningBoards = getWinningBoards newState.CalledNumbers newState.Boards
             if winningBoards.Length > 0 then
-                Some (calledNumber, winningBoards)
+                Some (newState, winningBoards)
             else
                 play newState)
 
     play initialState
+
+let lastNumberCalled (state: BingoState) =
+    List.head state.CalledNumbers
 
 let calculateScore finalNumber (winningBoards: Board list) =
     0 // TODO
@@ -74,8 +103,8 @@ let solve () =
     let initialState = input |> Seq.toList |> parseBingoInput
     let result = startGame initialState
     match result with
-    | Some (finalNumber, winningBoards) ->
-        calculateScore finalNumber winningBoards
+    | Some (finalState, winningBoards) ->
+        calculateScore (lastNumberCalled finalState) winningBoards
     | None ->
         0
 
